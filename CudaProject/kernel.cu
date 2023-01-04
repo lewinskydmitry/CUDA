006b6528ff10b8cb7b5efc00f610a2c42ed9e857
lewinskydmitry <61576391+lewinskydmitry@users.noreply.github.com>
2023-01-04 23:19:56 +0100

    add GPU transpose matrix

--- CudaProject/kernel.cu
+++ CudaProject/kernel.cu
@@ -3,9 +3,16 @@
 
 
 int main() {
-	Matrix rand_matrix_1 = Matrix::create_matrix(3, 4, 0, 5);
+	Matrix rand_matrix_1 = Matrix::create_matrix(2, 4, 0, 5);
 	Matrix rand_matrix_2 = Matrix::create_matrix(3, 3, 0, 5);
 
-	Matrix::equal(rand_matrix_1, rand_matrix_2);
+	rand_matrix_1.print();
+	std::cout << "\n";
+
+	Matrix A = MatTranspose(rand_matrix_1);
+	A.print();
+
+
+	Matrix::equal(A, Matrix::transpose(rand_matrix_1));
     return 0;
 }
--- MatrixOperations/MatTranspose.cu
+++ MatrixOperations/MatTranspose.cu
@@ -1,20 +1,19 @@
 #include "MatrixOperations.cuh"
 
-__global__ void copySharedMem(const Matrix init, Matrix result)
+__global__ void copySharedMem(Matrix odata, const Matrix idata)
 {
-    __shared__ float tile[BLOCK_SIZE * BLOCK_SIZE];
+    __shared__ double tile[BLOCK_SIZE * BLOCK_SIZE];
 
     int x = blockIdx.x * BLOCK_SIZE + threadIdx.x;
     int y = blockIdx.y * BLOCK_SIZE + threadIdx.y;
-    int width = gridDim.x * BLOCK_SIZE;
 
     for (int j = 0; j < BLOCK_SIZE; j += BLOCK_SIZE)
-        tile[(threadIdx.y + j) * BLOCK_SIZE + threadIdx.x] = init.data[(y + j) * width + x];
+        tile[(threadIdx.y + j) * BLOCK_SIZE + threadIdx.x] = idata.data[(y + j) * idata.width + x];
 
     __syncthreads();
 
     for (int j = 0; j < BLOCK_SIZE; j += BLOCK_SIZE)
-        result.data[(y + j) * width + x] = tile[(threadIdx.y + j) * BLOCK_SIZE + threadIdx.x];
+        odata.data[(y + j) * odata.width + x] = tile[(threadIdx.x + j) * BLOCK_SIZE + threadIdx.y];
 }
 
 
@@ -35,12 +34,11 @@ Matrix MatTranspose(Matrix A)
     // Invoke kernel
     dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
     dim3 dimGrid((A.width + dimBlock.x - 1) / dimBlock.x, (A.length + dimBlock.y - 1) / dimBlock.y);
-    copySharedMem << <dimGrid, dimBlock >> > (d_A, d_C);
+    copySharedMem << <dimGrid, dimBlock >> > (d_C, d_A);
 
     Matrix C;
     C.length = A.width;
     C.width = A.length;
-    size = A.length * A.width;
     C.data = new double[size];
 
     // Read C from device memory
